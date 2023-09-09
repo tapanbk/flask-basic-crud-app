68c1802a1e73157b8b5639e4fd9da0248e4e00be
tapanbk <tapan.bk@outcodesoftware.com>
2023-09-09 22:26:07 +0545

    [Add] Login page, plain layout and Login logout functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
@@ -12,8 +12,22 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
 
-def hash_password(password) :
-    return bcrypt.generate_password_hash('password').decode('utf-8')
+def logged_in(view_function):
+    def wrapper(*args, **kwargs):
+        if not session.get('username') and not session.get('is_logged_in'):
+            flash("UnAuthorized")
+            return redirect(url_for('login'))
+        return view_function(*args, **kwargs)
+    return wrapper
+
+
+def hash_password(password):
+    return bcrypt.generate_password_hash(password).decode('utf-8')
+
+
+def check_if_password_is_valid(database_password, input_password):
+    return bcrypt.check_password_hash(database_password, input_password)
+
 
 class User(db.Model):
     id = db.Column('user_id', db.Integer, primary_key=True)
@@ -36,15 +50,14 @@ class User(db.Model):
         return f"{self.first_name} {self.last_name}"
 
 
-def check_if_password_is_valid(database_password, input_password):
-    return bcrypt.check_password_hash(hash_password(database_password), input_password)
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def home():
         return render_template('home.html')
 
 
-@app.route('/create/', methods=['POST', 'GET'])
+@app.route('/create/', methods=['POST', 'GET'], endpoint="create")
+@logged_in
 def create():
     form = {}
     if request.method == "POST":
@@ -55,7 +68,7 @@ def create():
             db.session.commit()
             message = 'Successfully inserted'
             flash(message)
-            return redirect(url_for('list'))
+            return redirect(url_for('home'))
         except Exception as e:
             message = 'Failed'
             flash(message)
@@ -69,13 +82,9 @@ def details(userId):
     return render_template('details.html', user=user)
 
 
-@app.route('/list')
-def list():
-    users = User.query.all()
-    return render_template('list.html', users=users)
 
-
-@app.route('/edit/<int:userId>/', methods=['GET',"POST"])
+@app.route('/edit/<int:userId>/', methods=['GET',"POST"], endpoint="userEdit")
+@logged_in
 def userEdit(userId):
     # user_instance = User.query.filter_by(id=userId).first()
     user_instance = db.get_or_404(User, userId)
@@ -85,7 +94,8 @@ def userEdit(userId):
             user_instance.first_name = form.get('first_name')
             user_instance.last_name = form.get('last_name')
             user_instance.email = form.get('email')
-            user_instance.password = form.get('password')
+            if form.get('password'):
+                user_instance.password = hash_password(form.get('password'))
             user_instance.address = form.get('address')
             user_instance.phone = form.get('phone')
             db.session.commit()
@@ -97,6 +107,12 @@ def userEdit(userId):
             flash(message)
         return render_template('edit.html', user=form)
     return render_template('edit.html', user=user_instance)
+
+@app.route('/list', endpoint='list')
+@logged_in
+def list():
+    users = User.query.all()
+    return render_template('list.html', users=users)
 
 
 @app.route('/user/<int:userId>/', methods=['GET', "POST"])
@@ -113,6 +129,41 @@ def userDelete(userId):
             message = 'Failed'
             flash(message)
     return render_template('delete.html', user=user_instance)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    user_details = {
+        "email": ""
+    }
+    if request.method == 'GET':
+        return render_template('login.html', user_details=user_details)
+
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user_details["email"] = email
+
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            flash("Credentials does not match")
+            return render_template('login.html', user_details=user_details)
+        if not check_if_password_is_valid(user.password, password):
+            flash("Credentials does not match")
+            return render_template('login.html', user_details=user_details)
+        session['is_logged_in'] = True
+        session['full_name'] = f'{user.first_name} {user.last_name}'
+        flash("Successfully Logged in")
+        return redirect(url_for('list'))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('is_logged_in', None)
+    session.pop('username', None)
+    flash("Successfully Logged out")
+    return redirect(url_for('login'))
+
 
 @app.errorhandler(404)
 def invalid_route(e):
